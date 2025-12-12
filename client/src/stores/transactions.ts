e349527d305506d09134fd58b4a951e340c59698
MAtterholt <matterholt.dev@gmail.com>
2025-12-11 21:53:37 -0500

    some refactoring

---
+++ client/src/stores/transactions.ts
@@ -0,0 +1,23 @@
+export const entries = [
+  {
+    id: 1,
+    account: 'Account 1',
+    item: 'Item 1',
+    date: '2023-01-01',
+    amount: { dollar: '100', cent: '34', moneyFlow: 'income' },
+  },
+  {
+    id: 2,
+    account: 'Account 2',
+    item: 'Item 2',
+    date: '2023-01-02',
+    amount: { dollar: '300', cent: '34', moneyFlow: 'outgoing' },
+  },
+  {
+    id: 3,
+    account: 'Account 3',
+    item: 'Item 3',
+    date: '2023-01-03',
+    amount: { dollar: '341', cent: '35', moneyFlow: 'income' },
+  },
+]
--- client/src/utils/moneyFormatter.ts
+++ client/src/utils/moneyFormatter.ts
@@ -1,10 +1,3 @@
 export function formatMoney(amount: { dollar: string; cent: string; moneyFlow: string }) {
-  const dollar = amount.dollar
-  const cent = amount.cent
-  const moneyFlow = amount.moneyFlow
-
-  const formattedAmount = `${dollar}.${cent}`
-  const formattedMoneyFlow = moneyFlow === 'income' ? '+' : '-'
-
-  return `${formattedMoneyFlow}${formattedAmount}`
+  return `${amount.moneyFlow === 'income' ? '+' : '-'}${amount.dollar}.${amount.cent}`
 }
